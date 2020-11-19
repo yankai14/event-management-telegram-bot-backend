0d835b651332bb66c44a186f09ba9a68af3fb75f
Karma-98 <limyk2014@gmail.com>
2020-11-19 19:36:12 +0800

    - Modified event models attributes' arguments - Wrote minimal test for views - Added serializers module

--- lms/models/event_models.py
+++ lms/models/event_models.py
@@ -3,20 +3,20 @@ from django.db import models
 
 class Event(models.Model):
 
-    eventCode = models.CharField(max_length=30)
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    eventCode = models.CharField(max_length=30, blank=False, null=True)
+    name = models.CharField(max_length=100, blank=False, null=True)
+    description = models.TextField(default="")
 
 
 class EventInstance(models.Model):
     ''' An EventInstance is a many to one relation with Event. 
     Eg A module has many tutorial groups. '''
     
-    eventInstanceCode = models.CharField(max_length=30)
-    startDate = models.DateField()
-    endDate = models.DateField()
-    location = models.CharField(max_length=250)
-    dates = models.JSONField()
+    eventInstanceCode = models.CharField(max_length=30, blank=False, null=True)
+    startDate = models.DateField(blank=False, null=True)
+    endDate = models.DateField(blank=False, null=True)
+    location = models.CharField(max_length=250, blank=False, null=True)
+    dates = models.JSONField(blank=False, null=True)
     isCompleted = models.BooleanField(default=False)
 
-    event = models.ForeignKey('Event', on_delete=models.SET_NULL)
+    event = models.ForeignKey('Event', null=True, on_delete=models.SET_NULL)
--- lms/tests/test_views.py
+++ lms/tests/test_views.py
@@ -1,2 +1,34 @@
-from rest_framework.test import APITestCase
-from lms.models import Event
+import json
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.urls import reverse
+from lms.views import EventView
+
+
+client = APIClient()
+
+
+class CreateEventViewTest(APITestCase):
+
+    def setUp(self):
+        self.validPayload = {
+            "eventCode": "T101",
+            "name": "testEvent1",
+            "description": "This is my description"
+        }
+
+        self.invalidPayload = {
+            "eventCode": "",
+            "name": "invalidTestEvent",
+            "description": "This is my description"
+        }
+
+    def test_valid_create_event(self):
+        response = client.post(
+            reverse("create-event"),
+            data=json.dumps(self.validPayload),
+            content_type='application/json'
+        )
+
+        self.assertEqual(json.loads(response.data), self.validPayload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
