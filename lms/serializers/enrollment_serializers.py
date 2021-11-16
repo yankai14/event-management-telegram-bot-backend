0f6ed1ecb4bcfbd33a0a38e915c411bbb38283a6
Jun Kai <ljlimjk10@gmail.com>
2021-11-16 22:46:04 +0800

    Refactor code and add enrollmentStatus to serializer

--- lms/serializers/enrollment_serializers.py
+++ lms/serializers/enrollment_serializers.py
@@ -5,25 +5,30 @@ from rest_framework.exceptions import ValidationError
 from backend.exception_classes import ModelObjectAlreadyExist
 from lms.models.user_models import User
 from lms.models.enrollment_models import UserEnrollment
-from lms.models.event_models import EventInstance
+from lms.models.event_models import Event, EventInstance
+from lms.serializers.event_serializers import EventInstanceSerializer
 
+# Added enrollmentStatus as read_only field
 
 class EnrollmentSerializer(serializers.ModelSerializer):
 
     username = serializers.CharField(max_length=100, write_only=True)
     eventInstanceCode = serializers.CharField(max_length=100, write_only=True)
     user = serializers.StringRelatedField()
-    eventInstance = serializers.StringRelatedField()
-
+    eventInstance  = EventInstanceSerializer(read_only=True)
+    
     class Meta:
         model = UserEnrollment
         fields = "__all__"
-        extra_kwargs = {"user": {"read_only": True}, "eventInstance": {"read_only": True}}
-
+        extra_kwargs = {"user": {"read_only": True}, "enrollmentStatus": {"read_only": True}}
+        depth = 1
     def create(self, validated_data):
         if self.is_valid():
             user = get_object_or_404(User, username=validated_data.get("username", None))
-            eventInstance = get_object_or_404(EventInstance, eventInstanceCode=validated_data.get("eventInstanceCode", None))
+            eventInstanceCode = validated_data.get("eventInstanceCode", None)
+            if eventInstanceCode:
+                testEventInstance = EventInstance.objects.filter(eventInstanceCode=eventInstanceCode)
+                eventInstance = get_object_or_404(testEventInstance)
 
             userCriteria = Q(user=user)
             eventInstanceCriteria = Q(eventInstance=eventInstance)
