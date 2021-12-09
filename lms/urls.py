83b3d25634bdabca98129324c66c4e63feea1cba
Jun Kai <ljlimjk10@gmail.com>
2021-12-09 15:35:01 +0800

    Added API endpoints for enrollment and payment

--- lms/urls.py
+++ lms/urls.py
@@ -18,9 +18,16 @@ urlpatterns = [
 
     path("enrollment", views.enrollment_views.EnrollmentViewSet.as_view(), name="enrollment-view"),
     path("enrollment/<int:pk>", views.enrollment_views.EnrollmentViewSet.as_view(), name="enrollment-view"),
+    path("enrollment/<str:user__username>/<str:eventInstance__eventInstanceCode>", views.enrollment_views.EnrollmentViewSet.as_view(), name="enrollment-view"),
 
     path('event-instance-feedback/', views.feedback_views.EventInstanceFeedbackViewSet.as_view(), name="event-instance-feedback-view"),
     path('event-instance-feedback/<int:pk>', views.feedback_views.EventInstanceFeedbackViewSet.as_view(), name="event-instance-feedback-view"),
+
+    path('event-payment/', views.stripe_checkout_views.StripeCheckoutViewSet.as_view(), name="user-payment"),
+    path('event-payment-gateway', views.stripe_checkout_views.StripeCheckoutGatewayViewSet.as_view(), name="event-payment-gateway"),
+    path('event-payment/webhook', views.stripe_checkout_views.StripeCheckoutWebhook.as_view(), name="event-payment-webhook"),
+    path('stripe-success', views.stripe_success_page, name="stripe-success-page"),
+    path('stripe-failed', views.stripe_failed_page, name='stripe-failed-page'),
 
     path("auth/user", views.user_views.UserViewSet.as_view(), name="user-view"),
     path("auth/user/<str:username>", views.user_views.UserViewSet.as_view(), name="user-view"),
